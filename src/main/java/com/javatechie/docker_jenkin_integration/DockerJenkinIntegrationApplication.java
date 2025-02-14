a244f7e24fc9438bc2c21c4b679c6f5a292c5e7b
umereddy <umamaheshmereddy@gmail.com>
2025-02-14 13:55:56 -0600

    first commit

---
+++ src/main/java/com/javatechie/docker_jenkin_integration/DockerJenkinIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkin_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/docker_jenkin_integration/DockerJenkinIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkin_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
